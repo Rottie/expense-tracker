237c706a4099d0b91b2c018890814c70728a574b
Rottie <kingtang2180@gmail.com>
2021-07-20 15:24:09 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const app = express()
+
 const port = 3000
 
 // // 引用路由器
@@ -7,6 +7,22 @@ const port = 3000
 
 // // 將 request 導入路由器
 // app.use(routes)
+const mongoose = require('mongoose') // 載入 mongoose
+
+const app = express()
+mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 
 app.get('/', (req, res) => {
     
